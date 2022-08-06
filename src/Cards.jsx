e2fbf54ad44f881a6e42e0ff53da306f6f58586f
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2022-08-06 17:47:33 +0530

    React Props

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -11,6 +11,7 @@ function Cards(props){
                             <span className="card__category">{props.title}</span>
                             <h3 className="card__title">{props.seriesName}</h3>
                             <a href={props.link} target="_blank">
+                                <button>Watch Now</button>
                             </a>
                         </div>                
                     </div>
