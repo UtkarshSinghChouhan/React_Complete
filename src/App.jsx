819919dd619c19a2369775c8e3e58e1a642d3661
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2022-08-07 23:14:38 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,17 @@ function App(){
         <>
             <h1 className='heading_style'> List of Top 5 Netflix Series </h1>
 
-            <Cards imgsrc = {seriesData[0].imgsrc}
-                   title = {seriesData[0].title}
-                   seriesName = {seriesData[0].seriesName}
-                   link = {seriesData[0].link}
-            />
+            {seriesData.map((ele, index, array) => {
 
-            
-            <Cards imgsrc = {seriesData[1].imgsrc}
-                   title = {seriesData[1].title}
-                   seriesName = {seriesData[1].seriesName}
-                   link = {seriesData[1].link}
-            />
+                return(
+                    <Cards imgsrc = {ele.imgsrc}
+                           title = {ele.title}
+                           seriesName = {ele.seriesName}
+                           link = {ele.link}
+                    />
+                )
 
-            
-            <Cards imgsrc = {seriesData[2].imgsrc}
-                   title = {seriesData[2].title}
-                   seriesName = {seriesData[2].seriesName}
-                   link = {seriesData[2].link}
-            />
-
-
-            <Cards imgsrc = {seriesData[3].imgsrc}
-                   title = {seriesData[3].title}
-                   seriesName = {seriesData[3].seriesName}
-                   link = {seriesData[3].link}
-            />
-
-
-            <Cards imgsrc = {seriesData[4].imgsrc}
-                   title = {seriesData[4].title}
-                   seriesName = {seriesData[4].seriesName}
-                   link = {seriesData[4].link}
-            />
-
-
-            <Cards imgsrc = {seriesData[5].imgsrc}
-                   title = {seriesData[5].title}
-                   seriesName = {seriesData[5].seriesName}
-                   link = {seriesData[5].link}
-            />
+            })}
         </>
 
     );
