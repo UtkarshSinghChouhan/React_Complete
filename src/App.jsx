dfa97a315443a28e40ca213b74f7e9d4efd5a653
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2022-08-19 09:38:18 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-// let count = 0;
-
-
-
-
 const App = () => {
-    const IncreCount = () => {
-        setCount(++count);
-    }
 
-    let [count, setCount] = useState(0);
+    let Time = new Date().toLocaleTimeString();
+
+    const [initialTime, currentTime] = useState(Time);
+
+    const updateTime = () => {
+        // Updating the value of time-string
+        Time = new Date().toLocaleTimeString();
+
+        // Now currentTime() method will update the 'initialTime'
+        currentTime(Time)
+    }
     
     return(
         <>
-            <h1 className='heading'> {count} </h1>
-            <button onClick={IncreCount}> Click Me </button>
+            <h1 className='heading'> {initialTime} </h1>
+            <button onClick={updateTime}> Get Time</button>
         </>
     );
 }
