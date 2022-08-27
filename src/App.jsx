a2dcc49dafdbb69bd5fe43ae478345cd0d2364df
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2022-08-27 12:47:06 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './index.css';
-
-// let count = 0;
-
-
 
 
 const App = () => {
-    const IncreCount = () => {
-        setCount(++count);
-    }
-
-    let [count, setCount] = useState(0);
     
+    const [count,setCount] = useState(0);
+    
+    const IncNum = () => {
+        setCount(count + 1);        
+    };
+
     return(
         <>
-            <h1 className='heading'> {count} </h1>
-            <button onClick={IncreCount}> Click Me </button>
+            <h1> {count} </h1>
+            <button onClick={IncNum}> Click Me </button>
         </>
     );
 }
 
 export default App;
+
+   
